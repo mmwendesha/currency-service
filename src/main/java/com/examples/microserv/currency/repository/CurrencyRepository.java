11b7db667a59457e8d2c9e2c9c818ce8dd3b34e8
mmwendesha <mmwendesha@gmail.com>
2019-07-18 11:27:52 +0530

    Added dependecy on POM and fixed  some typo on currencies end point

--- src/main/java/com/examples/microserv/currency/repository/CurrencyRepository.java
+++ src/main/java/com/examples/microserv/currency/repository/CurrencyRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.examples.microserv.currency.model.Currency;
-@RepositoryRestResource(path="currecies", collectionResourceRel="currecies")
+@RepositoryRestResource(path="currencies", collectionResourceRel="currencies")
 public interface CurrencyRepository extends PagingAndSortingRepository<Currency, Integer> {
 
 }
